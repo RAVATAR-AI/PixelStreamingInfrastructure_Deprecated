69aa62aec36da5b3a7dd5bbb8299f4c9a7b36fcd
Yurii Kniazyk <43587150+YuriiKniazyk@users.noreply.github.com>
2024-11-11 11:56:30 +0200

    Update localStrategy.js
    Signed-off-by: Yurii Kniazyk <43587150+YuriiKniazyk@users.noreply.github.com>

--- SignallingWebServer/modules/authentication/strategies/localStrategy.js
+++ SignallingWebServer/modules/authentication/strategies/localStrategy.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('./db');
 
 module.exports = new LocalStrategy((username, password, callback) => {
